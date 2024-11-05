8306db2f05fe5bd6ada60eef80e0937696bf825f
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2024-11-05 17:17:51 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const { ethers } = require("ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    hardhat: {
-      chainId: 31337,
-    },
-    // open_chain: {
-    //   url: process.env.RPC_URL,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // hardhat: {
+    //   chainId: 31337,
     // },
+    polygon_amoy: {
+      url: process.env.RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      maxFeePerGas: ethers.utils.parseUnits("30", "gwei").toNumber(), // Adjust based on network conditions
+      maxPriorityFeePerGas: ethers.utils.parseUnits("2.5", "gwei").toNumber(), // Increase if needed
+    },
   },
 };
